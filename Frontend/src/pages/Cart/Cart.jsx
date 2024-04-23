be277b71a5d9fb08c661aa0c3ce772074b3d89e5
HAXAZE <28skofficial01@gmail.com>
2024-04-23 23:44:11 +0530

    added product removal from cart

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -23,14 +23,13 @@ const Cart = () => {
           if (cartItems[item._id] > 0) {
             return (
               <div>
-                <div className='cart-items-title cart-items-items'>
+                <div className='cart-items-title cart-items-item'>
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price * cartItems[item._id]}</p>
-                  <p className='cross'></p>
-                  <p>x</p>
+                  <p onClick={()=>removeFromCart(item._id)} className='cross'>x </p>
                 </div>
                 <hr />
               </div>
