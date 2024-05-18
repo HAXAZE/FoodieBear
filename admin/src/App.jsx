a29a07e50e5ce372338a7f5faabaa8a5cfc6edd0
HAXAZE <28skofficial01@gmail.com>
2024-05-18 10:31:34 +0530

    adding (add,list and orders logic for admin)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
 const App = () => {
   return (
     <div>
@@ -8,6 +14,11 @@ const App = () => {
       <hr/>
       <div className="app-content">
       <Sidebar/>
+      <Routes>
+        <Route path ="/add" element={<Add/>}/>
+        <Route path ="/list" element={<List/>}/>
+        <Route path ="/orders" element={<Orders/>}/>
+      </Routes>
       </div>
     </div>
   )
