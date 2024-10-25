b2d42d7352ed05707890ff5642a482e1081d4fc6
HAXAZE <28skofficial01@gmail.com>
2024-10-26 01:02:00 +0530

    FILE PATH

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import LoginPopup from './components/LoginPopup/LoginPopup'
